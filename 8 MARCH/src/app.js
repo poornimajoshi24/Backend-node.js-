532b6a52d21b57122c4deaac3a05fe4a31138cc2
poornimajoshi24 <joshipoornima969@gmail.com>
2026-03-09 23:09:39 +0530

    second commit

--- 8 MARCH/src/app.js
+++ 8 MARCH/src/app.js
@@ -27,11 +27,5 @@ app.delete("/notes/:mama", (req, res) => {
   });
 });
 
-app.patch("/notes/:index",(req,res)=>{
-    notes[req.params.index]=req.body.description
-    res.status(200).json({
-        message:"note updated successfully"
-    })
-})
 
 module.exports = app;
