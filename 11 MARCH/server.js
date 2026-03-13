772bfad77299a84cb25158003bb4201d66895a67
poornimajoshi24 <joshipoornima969@gmail.com>
2026-03-13 14:49:03 +0530

    inital commit

---
+++ 11 MARCH/server.js
@@ -0,0 +1,11 @@
+// server  ko start aur database se connect karta hai 
+const app=require('./src/app');
+const mongoose=require("mongoose")
+function connectTODb(){
+    mongoose.connect("mongodb+srv://joshipoornima969_db_user:Poornima123@cluster0.rhw72pt.mongodb.net/abc")
+    console.log("connected to database")
+}
+connectTODb();
+app.listen(3000,()=>{
+    console.log('server is running on port 3000')
+})
--- 11 MARCH/src/app.js
+++ 11 MARCH/src/app.js
@@ -1,1 +1,5 @@
 // ye server create aur configure karta hai 
+
+const express=require('express');
+const app=express()
+module.exports=app;
